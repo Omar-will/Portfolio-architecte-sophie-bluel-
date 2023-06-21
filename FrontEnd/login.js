f57dd34313f9121a08de2671af14724ed1624f74
Omar-will <omar.bengoudifa@outlook.fr>
2023-06-21 13:44:34 +0200

    first commit

---
+++ FrontEnd/login.js
@@ -0,0 +1,152 @@
+// Création de l'en-tête
+const header = document.createElement("header");
+const h1 = document.createElement("h1");
+h1.innerText = "Sophie Bluel";
+const span = document.createElement("span");
+span.innerText = "Architecte d'intérieur";
+h1.appendChild(span);
+header.appendChild(h1);
+
+const nav = document.createElement("nav");
+const ul = document.createElement("ul");
+const liProjets = document.createElement("li");
+liProjets.innerText = "projets";
+const liContact = document.createElement("li");
+liContact.innerText = "contact";
+const liLogin = document.createElement("li");
+const link = document.createElement("a");
+link.innerText = "login";
+link.href = "./login.html";
+link.style.textDecoration = "none"; 
+link.style.color = "inherit";
+const liInstagram = document.createElement("li");
+const imgInstagram = document.createElement("img");
+imgInstagram.src = "./assets/icons/instagram.png";
+imgInstagram.alt = "Instagram";
+imgInstagram.style.width = '20px';
+imgInstagram.style.height = 'auto'
+liInstagram.appendChild(imgInstagram);
+
+liLogin.appendChild(link);
+ul.appendChild(liProjets);
+ul.appendChild(liContact);
+ul.appendChild(liLogin);
+ul.appendChild(liInstagram);
+nav.appendChild(ul);
+header.appendChild(nav);
+
+const main = document.createElement('main');
+const section = document.createElement('section');
+section.className = 'log_in';
+section.id = 'contact';
+const h2 = document.createElement('h2');
+h2.textContent = 'Log In';
+const form = document.createElement('form');
+form.action = '#';
+form.method = 'post';
+const label1 = document.createElement('label');
+label1.htmlFor = 'email';
+label1.textContent = 'Email';
+const input1 = document.createElement('input');
+input1.type = 'email';
+input1.name = 'email';
+input1.id = 'email';
+const label2 = document.createElement('label');
+label2.htmlFor = 'password';
+label2.textContent = 'Password';
+const input2 = document.createElement('input');
+input2.type = 'password';
+input2.name = '';
+input2.id = 'password';
+input2.autocomplete = 'off';
+const input3 = document.createElement('input');
+input3.type = 'submit';
+input3.value = 'Se connecter';
+const p = document.createElement('p');
+p.textContent = 'Mot de passe oublié';
+const errorSpan = document.createElement('span');
+errorSpan.className = 'error';
+form.appendChild(label1);
+form.appendChild(input1);
+form.appendChild(label2);
+form.appendChild(input2);
+form.appendChild(input3);
+section.appendChild(h2);
+section.appendChild(form);
+section.appendChild(p);
+section.appendChild(errorSpan);
+main.appendChild(section);
+
+const footer = document.createElement('footer');
+footer.className = 'login';
+const footerNav = document.createElement('nav');
+const footerUl = document.createElement('ul');
+const footerLi = document.createElement('li');
+footerLi.textContent = 'Mentions Légales';
+footer.style.backgroundColor = 'white';
+footer.style.padding = '7px';
+footerUl.appendChild(footerLi);
+footerNav.appendChild(footerUl);
+footer.appendChild(footerNav);
+
+document.body.appendChild(header);
+document.body.appendChild(main);
+document.body.appendChild(footer);
+
+
+// ------- Requéte API ---------------------------
+
+function requestLogin() {
+    return fetch("http://localhost:5678/api/users/login", {
+        // indique que la requête doit être effectuée avec la méthode HTTP POST
+        method: 'POST',
+        headers: {
+            "Content-type": "application/json"
+        },
+        body: JSON.stringify({
+            "email": stockInputEmail,
+            "password": stockInputPassword,
+        })
+    });
+};
+// ---- Récupération du dom -----------------------------------------
+const inputPassword = document.querySelector('input[type="password"]');
+const inputEmail = document.querySelector('input[type="email"]');
+const submit = document.querySelector('input[type="submit"]');
+const errorDisplay = document.querySelector('.error');
+const login = document.getElementById("login")
+//------ Variable de stockage valeur 
+
+let stockInputPassword = inputPassword.value;
+let stockInputEmail = inputEmail.value;
+//--------- Evénement click formulaire + check 
+
+submit.addEventListener('click', (e) => {
+    e.preventDefault();
+    stockInputEmail = inputEmail.value;
+    stockInputPassword = inputPassword.value;
+    requestLogin()
+        .then((response) => response.json())
+        .then(login => {
+            console.log(login);
+            if (login.token) {
+                localStorage.setItem('token', login.token);
+                isUserLogged = true;
+                window.location.href = "./index.html";
+                console.log("Utilisateur connécté");
+            } else {
+                console.error("Le token n'a pas été trouvé");
+                errorDisplay.innerHTML = "Identifiant ou Mot de passe incorrect";
+            };
+        });
+});
+
+// ------ Récupération des données --------------
+
+inputEmail.addEventListener('input', (e) => {
+    console.log(e.target.value);
+
+});
+inputPassword.addEventListener('input', (e) => {
+    console.log(e.target.value);
+});
---
+++ js/script.js
@@ -0,0 +1,354 @@
+function fetchCategory() {
+  return fetch("http://localhost:5678/api/categories")
+    .then((response) => response.json())
+    .then((categories) => {
+      categoriesData = categories;
+      return categories;
+    });
+}
+function fetchWorks() {
+  return fetch("http://localhost:5678/api/works")
+    .then((response) => response.json())
+    .then((works) => {
+      worksData = works;
+      return works;
+    });
+}
+
+// --- fonction asynchrome afin d'attendre la récupération des données
+async function fetchData() {
+  const works = await fetchWorks();
+  worksData = works;
+  const categories = await fetchCategory();
+  displayProjectAndCategories(categories, works);
+}
+
+// Création de l'en-tête
+const header = document.createElement("header");
+const h1 = document.createElement("h1");
+h1.innerText = "Sophie Bluel";
+const span = document.createElement("span");
+span.innerText = "Architecte d'intérieur";
+h1.appendChild(span);
+header.appendChild(h1);
+
+const nav = document.createElement("nav");
+const ul = document.createElement("ul");
+const liProjets = document.createElement("li");
+liProjets.innerText = "projets";
+const liContact = document.createElement("li");
+liContact.innerText = "contact";
+const liLogin = document.createElement("li");
+const link = document.createElement("a");
+link.innerText = "login";
+link.href = "./login.html";
+link.style.textDecoration = "none"; 
+link.style.color = "inherit";
+const liInstagram = document.createElement("li");
+const imgInstagram = document.createElement("img");
+imgInstagram.src = "./assets/icons/instagram.png";
+imgInstagram.alt = "Instagram";
+imgInstagram.style.width = '20px';
+imgInstagram.style.height = 'auto'
+liInstagram.appendChild(imgInstagram);
+
+liLogin.appendChild(link);
+ul.appendChild(liProjets);
+ul.appendChild(liContact);
+ul.appendChild(liLogin);
+ul.appendChild(liInstagram);
+nav.appendChild(ul);
+header.appendChild(nav);
+
+// Création du contenu principal
+const main = document.createElement("main");
+
+// Section introduction
+const sectionIntroduction = document.createElement("section");
+sectionIntroduction.id = "introduction";
+
+const figure = document.createElement("figure");
+const img = document.createElement("img");
+img.src = "./assets/images/sophie-bluel.png";
+img.alt = "sophie-bluel";
+figure.appendChild(img);
+sectionIntroduction.appendChild(figure);
+
+const article = document.createElement("article");
+const h2Introduction = document.createElement("h2");
+h2Introduction.innerText = "Designer d'espace";
+const p1Introduction = document.createElement("p");
+p1Introduction.innerText =
+  "Je raconte votre histoire, je valorise vos idées. Je vous accompagne de la conception à la livraison finale du chantier.";
+const p2Introduction = document.createElement("p");
+p2Introduction.innerText =
+  "Chaque projet sera étudié en commun, de façon à mettre en valeur les volumes, les matières et les couleurs dans le respect de l’esprit des lieux et le choix adapté des matériaux. Le suivi du chantier sera assuré dans le souci du détail, le respect du planning et du budget.";
+const p3Introduction = document.createElement("p");
+p3Introduction.innerText =
+  "En cas de besoin, une équipe pluridisciplinaire peut-être constituée : architecte DPLG, décorateur(trice)";
+
+article.appendChild(h2Introduction);
+article.appendChild(p1Introduction);
+article.appendChild(p2Introduction);
+article.appendChild(p3Introduction);
+sectionIntroduction.appendChild(article);
+
+main.appendChild(sectionIntroduction);
+
+// Section portfolio
+const sectionPortfolio = document.createElement("section");
+sectionPortfolio.id = "portfolio";
+
+const h2Portfolio = document.createElement("h2");
+h2Portfolio.innerText = "Mes Projets";
+sectionPortfolio.appendChild(h2Portfolio);
+
+const divGallery = document.createElement("div");
+divGallery.className = "gallery";
+
+const divItems = document.createElement("div");
+divItems.className = "items";
+
+const span1 = document.createElement("span");
+span1.innerText = "Tous";
+
+const span2 = document.createElement("span");
+span2.innerText = "Objets";
+
+const span3 = document.createElement("span");
+span3.innerText = "Appartements";
+
+const span4 = document.createElement("span");
+span4.innerText = "Hôtels & restaurants";
+
+divItems.appendChild(span1);
+divItems.appendChild(span2);
+divItems.appendChild(span3);
+divItems.appendChild(span4);
+sectionPortfolio.appendChild(divItems);
+
+// Ajoute une classe active au span cliqué
+divItems.addEventListener("click", function (event) {
+  // Vérifie si l'élément cliqué est un span
+  if (event.target.tagName === "SPAN") {
+    // Supprime la classe active de tous les spans
+    const spans = divItems.querySelectorAll("span");
+    spans.forEach(function (span) {
+      span.classList.remove("active");
+    });
+    // Ajoute la classe active au span cliqué
+    event.target.classList.add("active");
+  }
+});
+
+// ...
+
+// Fonction de filtrage des images
+function filterImages(category) {
+  const figures = divGallery.querySelectorAll("figure");
+
+  // Parcours de toutes les figures pour afficher ou masquer les images
+  figures.forEach(function (figure) {
+    const img = figure.querySelector("img");
+    const alt = img.alt.toLowerCase();
+
+    // Affiche les images correspondant à la catégorie sélectionnée
+    if (category === "tous" || alt.includes(category)) {
+      figure.style.display = "block";
+    } else {
+      figure.style.display = "none";
+    }
+  });
+}
+
+// Ajoutez un gestionnaire d'événement au conteneur des spans
+divItems.addEventListener("click", function (event) {
+  if (event.target.tagName === "SPAN") {
+    const spans = divItems.querySelectorAll("span");
+    spans.forEach(function (span) {
+      span.classList.remove("active");
+    });
+
+    event.target.classList.add("active");
+
+    // Obtenez la catégorie du span cliqué
+    const category = event.target.innerText.toLowerCase();
+
+    // Filtrez les images en fonction de la catégorie
+    filterImages(category);
+  }
+});
+
+// Fonction de filtrage des images
+function filterImages(category) {
+  const figures = divGallery.querySelectorAll("figure");
+
+  // Parcours de toutes les figures pour afficher ou masquer les images
+  figures.forEach(function (figure) {
+    const img = figure.querySelector("img");
+    const alt = img.alt.toLowerCase();
+
+    // Affiche les images correspondant à la catégorie sélectionnée
+    if (
+      category === "tous" ||
+      (category === "objets" &&
+        (alt === "abajour tahina" || alt === "structures thermopolis")) ||
+      (category === "appartements" &&
+        (alt === "appartement paris v" ||
+          alt === "appartement paris x" ||
+          alt === "appartement paris xviii")) ||
+      (category === "hôtels & restaurants" &&
+        (alt === "restaurant sushisen - londres" ||
+          alt === "villa “la balisiere” - port louis" ||
+          alt === "bar “lullaby” - paris" ||
+          alt === "hotel first arte - new delhi"))
+    ) {
+      figure.style.display = "block";
+    } else {
+      figure.style.display = "none";
+    }
+  });
+}
+
+const projects = [
+  {
+    src: "assets/images/abajour-tahina.png",
+    alt: "Abajour Tahina",
+    figcaption: "Abajour Tahina",
+  },
+  {
+    src: "assets/images/appartement-paris-v.png",
+    alt: "Appartement Paris V",
+    figcaption: "Appartement Paris V",
+  },
+  {
+    src: "assets/images/restaurant-sushisen-londres.png",
+    alt: "Restaurant Sushisen - Londres",
+    figcaption: "Restaurant Sushisen - Londres",
+  },
+  {
+    src: "assets/images/la-balisiere.png",
+    alt: "Villa “La Balisiere” - Port Louis",
+    figcaption: "Villa “La Balisiere” - Port Louis",
+  },
+  {
+    src: "assets/images/structures-thermopolis.png",
+    alt: "Structures Thermopolis",
+    figcaption: "Structures Thermopolis",
+  },
+  {
+    src: "assets/images/appartement-paris-x.png",
+    alt: "Appartement Paris X",
+    figcaption: "Appartement Paris X",
+  },
+  {
+    src: "assets/images/le-coteau-cassis.png",
+    alt: "Pavillon “Le coteau” - Cassis",
+    figcaption: "Pavillon “Le coteau” - Cassis",
+  },
+  {
+    src: "assets/images/villa-ferneze.png",
+    alt: "Villa Ferneze - Isola d’Elba",
+    figcaption: "Villa Ferneze - Isola d’Elba",
+  },
+  {
+    src: "assets/images/appartement-paris-xviii.png",
+    alt: "Appartement Paris XVIII",
+    figcaption: "Appartement Paris XVIII",
+  },
+  {
+    src: "assets/images/bar-lullaby-paris.png",
+    alt: "Bar “Lullaby” - Paris",
+    figcaption: "Bar “Lullaby” - Paris",
+  },
+  {
+    src: "assets/images/hotel-first-arte-new-delhi.png",
+    alt: "Hotel First Arte - New Delhi",
+    figcaption: "Hotel First Arte - New Delhi",
+  },
+];
+
+projects.forEach((project) => {
+  const figureProject = document.createElement("figure");
+  const imgProject = document.createElement("img");
+  imgProject.src = project.src;
+  imgProject.alt = project.alt;
+  const figcaptionProject = document.createElement("figcaption");
+  figcaptionProject.innerText = project.figcaption;
+
+  figureProject.appendChild(imgProject);
+  figureProject.appendChild(figcaptionProject);
+  divGallery.appendChild(figureProject);
+});
+
+sectionPortfolio.appendChild(divGallery);
+main.appendChild(sectionPortfolio);
+
+// Section contact
+const sectionContact = document.createElement("section");
+sectionContact.id = "contact";
+
+const h2Contact = document.createElement("h2");
+h2Contact.innerText = "Contact";
+sectionContact.appendChild(h2Contact);
+
+const pContact = document.createElement("p");
+pContact.innerText = "Vous avez un projet ? Discutons-en !";
+sectionContact.appendChild(pContact);
+
+const formContact = document.createElement("form");
+formContact.action = "#";
+formContact.method = "post";
+
+const labelName = document.createElement("label");
+labelName.innerText = "Nom";
+const inputName = document.createElement("input");
+inputName.type = "text";
+inputName.name = "name";
+inputName.id = "name";
+
+const labelEmail = document.createElement("label");
+labelEmail.innerText = "Email";
+const inputEmail = document.createElement("input");
+inputEmail.type = "email";
+inputEmail.name = "email";
+inputEmail.id = "email";
+
+const labelMessage = document.createElement("label");
+labelMessage.innerText = "Message";
+const textareaMessage = document.createElement("textarea");
+textareaMessage.name = "message";
+textareaMessage.id = "message";
+textareaMessage.cols = "30";
+textareaMessage.rows = "10";
+
+const inputSubmit = document.createElement("input");
+inputSubmit.type = "submit";
+inputSubmit.value = "Envoyer";
+
+formContact.appendChild(labelName);
+formContact.appendChild(inputName);
+formContact.appendChild(labelEmail);
+formContact.appendChild(inputEmail);
+formContact.appendChild(labelMessage);
+formContact.appendChild(textareaMessage);
+formContact.appendChild(inputSubmit);
+
+sectionContact.appendChild(formContact);
+main.appendChild(sectionContact);
+
+// Création du pied de page
+const footer = document.createElement("footer");
+const navFooter = document.createElement("nav");
+const ulFooter = document.createElement("ul");
+const liMentionsLegales = document.createElement("li");
+liMentionsLegales.innerText = "Mentions Légales";
+
+ulFooter.appendChild(liMentionsLegales);
+navFooter.appendChild(ulFooter);
+footer.appendChild(navFooter);
+
+// Ajout des éléments au document
+document.body.appendChild(header);
+document.body.appendChild(main);
+document.body.appendChild(footer);
